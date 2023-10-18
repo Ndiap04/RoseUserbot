88add33decd6d8282d60c334e1a86c22034261be
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2023-10-18 10:53:41 +0700

    Update start.py

--- Rose/plugins/start.py
+++ Rose/plugins/start.py
@@ -6,6 +6,7 @@ from pyrogram.enums import ParseMode
 from pyrogram import Client, filters, idle, enums
 from pyrogram.types import Message, User
 from ..modules.humanbytes import humanbytes
+from ..modules import db
 
 IF_TEXT = "<b>💬 MEMBER MENGIRIM PESAN</b>\n<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}\n\nBot @CuhatBarengBottyBot"
 IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
