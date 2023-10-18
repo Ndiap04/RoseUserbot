f7ee5805168b4ea02064e36df76f758553912e2b
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2023-10-18 10:07:28 +0700

    Update start.py

--- Rose/plugins/start.py
+++ Rose/plugins/start.py
@@ -10,8 +10,22 @@ from ..modules.humanbytes import humanbytes
 IF_TEXT = "<b>💬 MEMBER MENGIRIM PESAN</b>\n<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
 IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
 
-owner_id = var.OWNER_ID
+log_grub = var.LOG_GROUP_ID
 
+@bot.on_message(filters.command('start') & (filters.private | filters.group))
+async def start(bot, message):
+    chat_id = message.from_user.id
+    # Adding to DB
+    if not await db.is_user_exist(chat_id):
+        data = await bot.get_me()
+        BOT_USERNAME = data.username
+        await db.add_user(chat_id)
+        await bot.send_message(
+            LOG_GROUP_ID,
+            f"#NEWUSER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{BOT_USERNAME} !!",
+        )
+        return
+        
 @bot.on_message(filters.private & filters.text)
 async def pm_text(bot, message):
     if message.from_user.id == owner_id:
@@ -20,7 +34,7 @@ async def pm_text(bot, message):
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
     await bot.send_message(
-        chat_id=owner_id,
+        chat_id=log_grub,
         text=IF_TEXT.format(reference_id, info.first_name, message.text),
         parse_mode=ParseMode.HTML,
     )
