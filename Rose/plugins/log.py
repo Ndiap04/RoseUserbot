925f97dcdf3a9c0d8a50e85833ba7591041534c4
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2023-10-18 09:56:05 +0700

    Update log.py

--- Rose/plugins/log.py
+++ Rose/plugins/log.py
@@ -9,12 +9,12 @@ from ..modules.tools import get_arg
 from ..modules.vars import *
 
 
-@app.on_message(filters.group & filters.mentioned & filters.incoming & ~filters.bot & ~filters.via_bot)
+@bot.on_message(filters.group & filters.mentioned & filters.incoming & ~filters.bot & ~filters.via_bot)
 async def log_tagged_messages(client, message):
     chat_id = message.chat.id
     user_id = client.me.id
     botlog_chat_id = var.LOG_GROUP_ID
-    knl = f"📨<b><u>ANDA TELAH DI TAG</u></b>\n<b> • Dari : </b>{message.from_user.mention}"
+    knl = f"📨<b><u>ADMIN MENGIRIM PESAN</u></b>\n<b> • Dari : </b>{message.from_user.mention}"
     knl += f"\n<b> • Grup : </b>{message.chat.title}"
     knl += f"\n<b> • 👀 </b><a href = '{message.link}'>Lihat Pesan</a>"
     knl += f"\n<b> • Message : </b><code>{message.text}</code>"
